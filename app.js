cf3f5cdb6c242a2ca41f8f32f4bb326cd2c03c26
Joshua Chris <hello@digitalmax360.com>
2019-09-24 05:03:26 +0100

    JavaScript Coding Exercises (Arrays & Objects) - first video

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Welcome")
+var loginVar = new Object();
+var code;
+
+function access(){
+    loginVar.accessCode = 'CS1';
+    loginVar['pin'] = 1000;
+
+    code = document.getElementById('access').value;
+
+    if(code == ''){
+        document.getElementById('print').innerHTML = 'INSERT ACCESS CODE!';
+    } else if (code == loginVar.accessCode){
+        document.getElementById('hide').style.display = 'none';
+        document.getElementById('print').innerHTML = "YOU'RE LOGGED IN!"
+        document.getElementById('reset').style.display = 'block';
+    }
+    else {
+        alert('ACCESS ERROR - Contact Administrator!!!')
+    }
+}
+
+function reset(){
+    location.reload();
+}
